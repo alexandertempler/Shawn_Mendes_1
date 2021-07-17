e1af76c4c5923381b60ed6c83b78a830afac88e2
alexandertempler <52668546+alexandertempler@users.noreply.github.com>
2021-07-16 20:12:32 -0500

    Add files via upload

---
+++ shaw-mendez/js/js-home.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+      $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+           breakpoint: 400,
+           settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+           }
+        }]
+    });
+});
